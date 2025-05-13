c98a80b6e5d000b0ca98daa8392136ebbc9118af
Adarsh Kumar <kumaradarsh.cse@gmail.com>
2025-05-13 23:56:43 +0530

    Two Pointer technique learnt

---
+++ TwoPointer/segregateZeroOne.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> arr = {1, 1, 0, 0, 1, 0};
+    int n = arr.size();
+
+    // Naive Approach
+    
+    // This is not two pointer, just old method that I have been using
+/*
+    int i = 0, j = 1;
+    
+    while (j < n) {
+        if (arr[i] == 0) i++;
+        else {
+            if (arr[j] == 0) {
+                std::swap(arr[i], arr[j]);
+                i++;
+            }
+        }
+        j++;
+    }
+*/
+/***********************************************************************************************/
+// Standard Approach (Learn this!!!)
+// Time complexity: O(N) since both pointers come closer to each other and traverse array in single pass
+// Space complexity: O(1) since we didn't create an extra space
+
+int left = 0, right = n-1;
+
+while (left < right) {
+    if (arr[left] == 0) left++;
+    else { // arr[left] == 1
+        if (arr[right] == 0) {
+            std::swap(arr[left], arr[right]);
+            left++, right--;
+        }
+        else { // arr[right] == 1
+            right--;
+        }
+    }
+}
+
+/**********************************************************************************************/
+    // My Approach
+    // Not recommended approach: (too abstract, though solves problem but unnecessary logic)
+
+/*
+    // Mistake I made:
+    
+        // I mistook arr[left] for left, but left is pointer which points to index.
+        // So, to compare the values, or do anything with array element at that index, we do arr[left]
+        // and NOT just "left"
+    
+
+    while (left < right) {
+        if (arr[left] == arr[right]) {
+            if (left == 0) left++;
+            else right--;
+        }
+        else { // left != right
+            if (arr[left] == 1) {
+                std::swap(arr[left], arr[right]);
+            }
+            left++, right--;
+        }
+    }
+*/
+
+
+    // Printing the array
+    for (int elem : arr) std::cout << elem << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ TwoPointer/twoSum.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+// binary search function
+int binarySearch(std::vector<int> arr, int target, int startingIndex) {
+    int low = startingIndex, high = arr.size()-1; 
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target) return mid;
+        else if (target > arr[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int main(){
+    std::vector<int> arr = {2,7,11,15,27};
+    int n = arr.size();
+    int target = 22;
+
+    // Method 1: Using brute-force
+/*
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = i+1; j < n; ++j) {
+            if (arr[i] + arr[j] == target) {
+                std::cout << arr[i] << " " << arr[j] << std::endl;
+                break;
+            }
+        }
+    }
+*/
+/************************************************************************************************************/
+    // Method 2: Using binary search
+    // Approach: for any element x, we need to find target-x in subarray and since entire array is sorted, we can apply binary search here to optimize the previous solution
+
+    // Time complexity: O(N logN)
+    // Space complexity: O(1) since not using any extra space
+
+/*
+    int i, targetIndex;
+    for (i = 0; i < n-1; ++i) {
+        int newTarget = target-arr[i];
+        // use binary search here to find newTarget in the subarray [i+1, n-1]
+        targetIndex = binarySearch(arr, newTarget, i+1);
+        if (targetIndex != -1) break;
+    }
+
+    std::cout << arr[i] << " " << arr[targetIndex] << std::endl;
+*/
+
+    // Method 3: Using Two pointer approach
+    // Note: First we need sorted array for this approach to work.
+    // Imp: left pointer increases the value and right pointer decreases the sum value
+
+    // Time complexity: O(N) since both pointers converse and thus traverse the array in single pass
+    // Space complexity: O(1)
+    int left = 0, right = n-1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum > target) right--;
+        else if (sum < target) left++;
+        else break;
+    }
+
+    std::cout << arr[left] << " " << arr[right] << std::endl;
+
+    return 0;
+}
