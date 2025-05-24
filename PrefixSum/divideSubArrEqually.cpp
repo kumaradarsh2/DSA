a36102f1183f5cb0d99ec15932c3c8850661edf4
Adarsh Kumar <kumaradarsh.cse@gmail.com>
2025-05-24 23:52:27 +0530

    Kadane's Algo learnt & Problems solved

--- PrefixSum/divideSubArrEqually.cpp
+++ PrefixSum/divideSubArrEqually.cpp
@@ -1,61 +1,52 @@
-// Divide array into Sub Arrays such that their sum is equal
+// Problem: Divide an Array into equal parts such that their sum is equal
 #include <iostream>
 #include <vector>
 
 int main() {
     std::vector<int> arr = {3,4,-2,5,8,20,-10,8};
     int n = arr.size();
+    bool canBeDivided = false;
+    // Method 1: Bruteforce Approach
+    // Time complexity: O(n*2n) = O(2n^2) = O(n^2)
+    // Space complexity: O(1) since no extra space being used
+    /*
+    
+    for (int i = 0; i < n-1; ++i) {
+        int sum1 = 0, sum2 = 0;
 
-    bool sumExists = false;
+        for (int j = 0; j <= i; ++j) sum1 += arr[j];
+        
+        for (int j = i+1; j < n; ++j) sum2 += arr[j];
 
-    // Method 1
-    // Time complexity: O(N*N) = O(N^2)
-    // Space complexity: O(1) since we are not using any extra space
-/*
-    for (int k = 0; k < n; k++) {
-        int prefixSum = 0, suffixSum = 0;
-        for (int i = 0; i < n; i++) {
-            if (i < k) prefixSum += arr[i];
-            else suffixSum += arr[i];
-        }
-        // Print statement for debugging
-        std::cout << "Prefix Sum: " << prefixSum << " Suffix Sum: " << suffixSum << std::endl;
-
-        // Compare sum here
-        if (prefixSum == suffixSum) {
-            sumExists = true;
-            std::cout << true << std::endl;
+        if (sum1 == sum2) {
+            canBeDivided = true;
+            std::cout << "Yes, Divided at index " << i << std::endl;
             break;
-        } 
+        }
     }
 
-*/
-/*********************************************************************************************/
-    // Method 2
-    // Time complexity: O(N)
-    // Space complexity: O(1)
+    
+    */
 
-    // Approach: 
-    // First we calculate total sum
+    // Method 2: Using Prefix Sum
+    // First calculate totalSum
     int totalSum = 0;
     for (int elem : arr) totalSum += elem;
 
     int prefixSum = 0;
     for (int i = 0; i < n-1; ++i) {
         prefixSum += arr[i];
-        int suffixSum = totalSum - prefixSum;
+        int ans = totalSum - prefixSum;
 
-        // Print statement for debugging
-        std::cout << "Prefix Sum: " << prefixSum << " Suffix Sum: " << suffixSum << std::endl;
-
-        if (prefixSum == suffixSum) {
-            sumExists = true;
-            std::cout << true << std::endl;
+        if (ans == prefixSum) {
+            canBeDivided = true;
+            std::cout << "Yes, Divided at index " << i << std::endl;
             break;
         }
     }
 
-    if (!sumExists) std::cout << false << std::endl;
+    if (!canBeDivided)
+            std::cout << "No, Can't be divided into equal sum" << std::endl;
 
     return 0;
 }
--- PrefixSum/largestSumSubArr.cpp
+++ PrefixSum/largestSumSubArr.cpp
@@ -4,12 +4,14 @@
 #include <climits>
 
 int main() {
-    std::vector<int> arr = {4,-6, 2, 8};
+    std::vector<int> arr = {-2,-4};
     int n = arr.size();
 
     // Generate all the subarrays and take the maximum sum
+    // Time complexity: O(N^3)
+    // Space complexity: O(1)
     int maxSum = INT_MIN;
-
+/*
     for (int size = 1; size <= n; ++size) {
         for (int i = 0; i <= n-size; ++i) {
             int subArrSum = 0;
@@ -18,6 +20,38 @@ int main() {
             }
             maxSum = std::max(subArrSum, maxSum);
         }
+    }
+*/
+
+/*************************************************************************************************/
+    // Method 2: 
+    // Time complexity: O(N^2)
+    // Space complexity: O(1)
+    /*
+    for (int i = 0; i < n; ++i) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += arr[j];
+            maxSum = std::max(sum, maxSum);
+        }
+    }
+    */
+/*************************************************************************************************/
+
+    // Method 3: Using Kandane's Algorithm
+    // Time complexity: O(N)
+    // Space complexity: O(1)
+
+    int prefix = 0;
+
+    for (int i = 0; i < n; ++i) {
+        prefix += arr[i];
+
+        maxSum = std::max(prefix, maxSum);
+
+        // Whenever we see negative prefix, make it zero 
+        //          -- Kandane's Algorithm
+        if (prefix < 0) prefix = 0;
     }
 
     std::cout << maxSum << std::endl;
